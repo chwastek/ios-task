3f2b9befa53ae958e21a0e50dd411c1f3a82107b
Filip Chwastowski <chwastek@gmail.com>
2021-06-28 00:55:11 +0200

    Adde UI test for correct title and description on main screen.

--- CampaignBrowserUITests/CampaignBrowserUITests.swift
+++ CampaignBrowserUITests/CampaignBrowserUITests.swift
@@ -10,9 +10,21 @@ class CampaignBrowserUITests: XCTestCase {
         application = XCUIApplication()
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_viewContainsCell_withCorrectTitleAndDescription() {
+        // Given
+        let firstTitle = "Kreative Wandideen"
+        let firstDescription = "Wir machen uns viele Gedanken, wenn es ums die Wahl der M\u{00f6}bel oder die passende Deko geht. Doch auch eine pers\u{00f6}nlich gestaltete Wand kann ein eindrucksvolles Wohn-Statement sein. Denn es gibt viele M\u{00f6}glichkeiten, sich auszutoben. Von praktischen Bilderleisten, die schnelle Ver\u{00e4}nderungen erlauben, \u{00fc}ber Memoboards bis zu leuchtenden Schriftz\u{00fc}gen \u{2013} diese kreativen Ideen machen Lust aufs Gestalten."
+        
+        // When
+        application.launch()
+        let cellTitles = application.staticTexts["title"]
+        let cellDescriptions = application.staticTexts["description"]
+        
+        XCTAssertTrue(cellTitles.firstMatch.waitForExistence(timeout: 1.0))
+        XCTAssertTrue(cellDescriptions.firstMatch.exists)
+        
+        XCTAssertEqual(cellTitles.firstMatch.label, firstTitle)
+        XCTAssertEqual(cellDescriptions.firstMatch.label, firstDescription)
     }
 
 }
